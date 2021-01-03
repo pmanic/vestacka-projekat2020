230ea04278ca9b3c229836ca90d3b1b09c2ec0d5
Petar <pera.manic@gmail.com>
2021-01-03 18:07:10 +0100

    Interfejs 50%

--- kreiranjeIPrikaz.lisp
+++ kreiranjeIPrikaz.lisp
@@ -2,34 +2,32 @@
 (setf brojevi '((0 0) (1 1) (2 2) (3 3) (4 4) (5 5) (6 6) (7 7) (8 8) (9 9) (A 10) (B 11) (C 12) (D 13) (E 14) (F 15)))
 
 
-(defun ispuniVrstu(el n brojac)
+;-------------------------------------------------------------KREIRANJE-------------------------------------------------------------
+
+(defun ispuniJedanRed(el n brojac)
 	(cond
 		( (= brojac 0) '())
-		( T (cons (ispuniListu el n) (ispuniVrstu el n (1- brojac) )) )		
+		( T (cons (ispuniStub el n) (ispuniJedanRed el n (1- brojac) )) )		
 	)
 )
 
-(defun izvuciBroj(lista)
-    (car (cdr lista))
-)
-
-(defun ispuniListu (el n) ;vraca listu 
+(defun ispuniStub (el n) ;vraca Stub 
 ;lista ima n elemenata i svi su isti kao parametar el
 	(cond
 		( (= n 0) '())
-		( T (cons el (ispuniListu el (1- n) )) )		
+		( T (cons el (ispuniStub el (1- n) )) )		
 	)
 )
 
-(defun kreirajTabluPom (el n brojac)
+(defun kreirajTablu (el n brojac)
     (cond
 		( (= brojac 0) '())
-		( T (cons (ispuniVrstu el n n) (kreirajTabluPom el n (1- brojac) )) )		
+		( T (cons (ispuniJedanRed el n n) (kreirajTablu el n (1- brojac) )) )		
 	)
 )
 
 (defun novaIgra (n igracXiliO)
-;X uvek igra prvi
+    ;X uvek igra prvi
 	(setq napotezu 'X)
     ;ako je igracXiliO postavljeno na 'X, znaci da covek igra prvi, a ako je postavljeno na 'O,
     ;znaci da racunar igra prvi
@@ -41,17 +39,33 @@
     (setq brojacPoteza '0)
 	(setq dimTable n)
 	
-   (setq tabla (kreirajTabluPom '- n n)) 
+    (setq tabla (kreirajTablu '- n n)) 
+    ;(let ((slova '(0 1 2 3 4 5 6 7 8 9 A B C D E F)))
+    ;(prikaziTablu tabla n slova)
 )
+
+;----------------------------------------------------------------------------------------------------------------------------------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;-------------------------------------------------------------PRIKAZ----------------------------------------------------------------
+
+
+
+;----------------------------------------------------------------------------------------------------------------------------------------
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;--------------------------------------------------------KRAJ IGRE I POTEZI-------------------------------------------------------------
 
 (defun krajIgre ()
     (eq brojacPoteza '64)
 )
 
-(defun nadjiVrstu (brVrste matrica)
+(defun nadjiJedanRed (brReda matrica)
     (cond
-        ((= brVrste 0) (car matrica))
-        (T (nadjiVrstu (1- brVrste) (cdr matrica)))
+        ((= brReda 0) (car matrica))
+        (T (nadjiJedanRed (1- brReda) (cdr matrica)))
         
     )
 )
@@ -66,13 +80,18 @@
     )
 )
 
+(defun izvuciBroj(lista)
+    (car (cdr lista))
+)
+
 (defun proveriPotezAtom (potez)
-    (proveriValidan (nadjiVrstu  (- (izvuciBroj(assoc potez brojevi)) (* (floor (izvuciBroj(assoc potez brojevi)) dimTable) dimTable) ) (nadjiVrstu (floor (izvuciBroj(assoc potez brojevi)) dimTable) tabla)) )
+    (proveriValidan (nadjiJedanRed  (- (izvuciBroj(assoc potez brojevi)) (* (floor (izvuciBroj(assoc potez brojevi)) dimTable) dimTable) ) (nadjiJedanRed (floor (izvuciBroj(assoc potez brojevi)) dimTable) tabla)) )
 )
 
 (defun proveriPotezLista (lista)
-    (proveriValidan (nadjiVrstu (izvuciBroj lista) (nadjiVrstu (car lista) tabla)))
+    (proveriValidan (nadjiJedanRed (izvuciBroj lista) (nadjiJedanRed (car lista) tabla)))
 )
+
 (defun potezValidan (potez)
     (cond
       ( (atom potez) (proveriPotezAtom potez))
@@ -80,12 +99,63 @@
     )
 )
 
+;----------------------------------------------------------------------------------------------------------------------------------------
+(defun vratiRed(lista brojac)
+    (cond
+        ((null lista) '())
+        (t (append (vratielemente (reverse (car lista)) brojac) (vratiRed (cdr lista) brojac)))
+    )
+)
+(defun vratielemente(lista brojac)
+    (cond
+        ((= brojac 0) '())
+        (t (cons (caar lista) (vratielemente (cdr lista) (1- brojac))))
+    )
+)
 
+(defun prikaziTablu(lista brojac)
+    (cond
+        ((null lista) '())
+        ; (T (cons (caar (cdddar lista))(prikaziTablu (cdr lista))))
+        (T (cons (append (list (append  (reverse (cdr (reverse (car lista)))) (list (cdr (car (reverse (car lista))))))) (prikaziTablu (cdr lista)))))
+
+    )
+)
+
+(defun brisiRed(lista brojac)
+    (cond
+        ((= brojac 0) lista)
+        (t (reverse (append (list (cdar lista)) (reverse (brisiRed (cdr lista) (1- brojac))))))
+    )
+)
+
+(defun brisiIzSvakogReda(lista brojac)
+    (cond
+        ((null lista) '())
+        (t (append (list (brisiRed (reverse (car lista)) brojac)) (brisiIzSvakogReda (cdr lista) brojac)))
+    )
+)
 
 (novaIgra 4 4)
-(setq tabla '(((X X X X) (O O O -) (- - - -) (- - - -)) ((- - - -) (- - - -) (- - - -) (- - - -)) ((- - - -) (- - - -) (- - - -) (- - - -))
- ((- - - -) (- - - -) (- - - -) (- - - -))))
-(print (potezValidan '1))
+(setq tablaa '(((25 41 53 61) (13 26 42 54) (5 14 27 43) (1 6 15 28)) 
+               ((29 44 55 62) (16 30 45 56) (7 17 31 46) (2 8 18 32)) 
+               ((33 47 57 63) (19 34 48 58) (9 20 35 49) (3 10 21 36))
+               ((37 50 59 64) (22 38 51 60) (11 23 39 52) (4 12 24 40))))
+
+; (print (caar (cdddar tablaa)))
+
+;(trace vratielemente)
+;(print (vratielemente (reverse '((25 41 53 61) (13 26 42 54) (5 14 27 43) (1 6 15 28))) '2))
+;(print (vratiRed '(((25 41 53 61) (13 26 42 54) (5 14 27 43) (1 6 15 28)) 
+                 ; ((29 44 55 62) (16 30 45 56) (7 17 31 46) (2 8 18 32)) 
+                 ; ((33 47 57 63) (19 34 48 58) (9 20 35 49) (3 10 21 36))
+                 ; ((37 50 59 64) (22 38 51 60) (11 23 39 52) (4 12 24 40))) '2))
+;(print (brisiRed '((25 41 53 61) (13 26 42 54) (5 14 27 43) (1 6 15 28)) '3))
+(print (brisiIzSvakogReda '(((25 41 53 61) (13 26 42 54) (5 14 27 43) (1 6 15 28)) 
+               ((29 44 55 62) (16 30 45 56) (7 17 31 46) (2 8 18 32)) 
+               ((33 47 57 63) (19 34 48 58) (9 20 35 49) (3 10 21 36))
+               ((37 50 59 64) (22 38 51 60) (11 23 39 52) (4 12 24 40))) '3))
+;(print (potezValidan '1))
 
 
 
